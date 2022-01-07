a7ee84c9b675e6635b66c62096b03c0eaf594774
liuyi <liuyi@gennlife.com>
2022-01-07 14:46:34 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,22 @@ import './registerServiceWorker'
 
 import { qiankunFn, naiveUi } from './utils'
 import Api from './api'
-import store from './store'
+<%_ if (isVuex) { _%>
+  import store from './store'
+<%_ } _%>
 import router from './router'
 import App from './App.vue'
 
 import './assets/stylus/Init.styl'
 
 const instance: any = createApp(App)
-  .use(Api)
-  .use(naiveUi)
-  .use(router)
-  .use(store)
-  .mount('#app')
+instance.use(Api)
+instance.use(naiveUi)
+instance.use(router)
+<%_ if (isVuex) { _%>
+  instance.use(store)
+<%_ } _%>
+instance.mount('#<%= packageName %>')
+
 
 qiankunFn(instance)
